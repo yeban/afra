2944d416472a1c4a3ee858c5454d9b0734228486
Harsh Gupta <gupta.harsh96@gmail.com>
2014-12-26 13:16:46 +0530

    get a test transcript
    This transcript was obtained by stringifying a transcript on the
    EditTrack.
    
    Signed-off-by: Harsh Gupta <gupta.harsh96@gmail.com>

--- tests/js/EditTrack.spec.js
+++ tests/js/EditTrack.spec.js
@@ -2,12 +2,14 @@ define([
         'JBrowse/Browser',
         'JBrowse/View/Track/DraggableHTMLFeatures',
         'JBrowse/FeatureSelectionManager',
-        'JBrowse/View/Track/EditTrack'
-        ], function (Browser, DraggableHTMLFeatures, FeatureSelectionManager, EditTrack) {
+        'JBrowse/View/Track/EditTrack',
+        'JBrowse/Model/SimpleFeature'
+        ], function (Browser, DraggableHTMLFeatures, FeatureSelectionManager, EditTrack,
+            SimpleFeature) {
 
 describe( "Edit Track", function() {
 
-    var baseURL = '/data/jbrowse/Solenopsis_invicta/Si_gnF'; 
+    var baseURL = '/data/jbrowse/Solenopsis_invicta/Si_gnF';
     var jbrowse = new Browser({
         containerID: 'genome',
         baseUrl: baseURL,
@@ -16,6 +18,98 @@ describe( "Edit Track", function() {
     });
 
     var editTrack;
+    var input_refSeq;
+    var transcript_data = {
+    "_uniqueID": "SimpleFeature_2",
+    "data": {
+        "end": 21389,
+        "name": "Si_estOR100817isotig20471",
+        "seq_id": "Si_gnF.scaffold02797",
+        "start": 16946,
+        "strand": 1,
+        "subfeatures": [
+            {
+                "_uniqueID": "SimpleFeature_2_3",
+                "data": {
+                    "end": 17116,
+                    "name": "Si_estOR100817isotig20471",
+                    "seq_id": "Si_gnF.scaffold02797",
+                    "start": 16946,
+                    "strand": 1,
+                    "type": "exon"
+                }
+            },
+            {
+                "_uniqueID": "SimpleFeature_2_317",
+                "data": {
+                    "end": 17116,
+                    "seq_id": "Si_gnF.scaffold02797",
+                    "start": 17116,
+                    "strand": 1,
+                    "type": "non_canonical_splice_site"
+                }
+            },
+            {
+                "_uniqueID": "SimpleFeature_2_318",
+                "data": {
+                    "end": 19075,
+                    "seq_id": "Si_gnF.scaffold02797",
+                    "start": 19075,
+                    "strand": 1,
+                    "type": "non_canonical_splice_site"
+                }
+            },
+            {
+                "_uniqueID": "SimpleFeature_2_4",
+                "data": {
+                    "end": 19197,
+                    "name": "Si_estOR100817isotig20471",
+                    "seq_id": "Si_gnF.scaffold02797",
+                    "start": 19075,
+                    "strand": 1,
+                    "type": "exon"
+                }
+            },
+            {
+                "_uniqueID": "SimpleFeature_2_5",
+                "data": {
+                    "end": 20446,
+                    "name": "Si_estOR100817isotig20471",
+                    "seq_id": "Si_gnF.scaffold02797",
+                    "start": 20258,
+                    "strand": 1,
+                    "type": "exon"
+                }
+            },
+            {
+                "_uniqueID": "SimpleFeature_2_6",
+                "data": {
+                    "end": 21106,
+                    "name": "Si_estOR100817isotig20471",
+                    "seq_id": "Si_gnF.scaffold02797",
+                    "start": 21002,
+                    "strand": 1,
+                    "type": "exon"
+                }
+            },
+            {
+                "_uniqueID": "SimpleFeature_2_7",
+                "data": {
+                    "end": 21389,
+                    "name": "Si_estOR100817isotig20471",
+                    "seq_id": "Si_gnF.scaffold02797",
+                    "start": 21298,
+                    "strand": 1,
+                    "type": "exon"
+                }
+            }
+        ],
+        "type": "transcript"
+    },
+    "normalized": true
+}
+
+    var transcript = new SimpleFeature(transcript_data);
 
     beforeEach(function(done) {
         setTimeout(function () {
