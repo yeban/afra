80947a8e6f06f92879af24da1cc611aada4ff87e
Harsh Gupta <gupta.harsh96@gmail.com>
2014-12-29 19:59:29 +0530

    Write a saner feature comparison function
    Signed-off-by: Harsh Gupta <gupta.harsh96@gmail.com>

--- www/JBrowse/Util/FeatureEquality.js
+++ www/JBrowse/Util/FeatureEquality.js
@@ -1,14 +1,22 @@
 define([
-        'underscore',
-        'JBrowse/Util/ImportFeature',
-        'JBrowse/Util/ExportFeature'
+        "underscore",
+        "JBrowse/Util/ImportFeature",
+        "JBrowse/Util/ExportFeature"
         ],
 function (_, importFeature, exportFeature) {
 
     return function(featureA, featureB) {
         // Strip a feature of parent, uniqueID and name
-        var a = JSON.parse(exportFeature(featureA));
-        var b = JSON.parse(exportFeature(featureB));
-        return _.isEqual(a, b);
-    };
-});
+        var a = JSON.parse(exportFeature(featureA))["data"];
+        var b = JSON.parse(exportFeature(featureB))["data"];
+        if ((a["start"] === b["start"] && a["end"] === b["end"]) && (a["seq_id"] === b["seq_id"] && a["strand"] === b["strand"])) {
+            if (!_.isUndefined(a["subfeatures"]) && !_.isUndefined(b["subfeatures"])) {
+                var common_subfeatures = _.intersection(a["subfeatures"], b["subfeatures"]);
+                if (common_subfeatures.length === a["subfeatures"].length) {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        return false; };});
