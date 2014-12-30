1970c210656f5bade95151541201ba818e88d18e
Harsh Gupta <gupta.harsh96@gmail.com>
2014-12-30 16:44:45 +0530

    fix sortAnnotationsByLocation comparison func
    Signed-off-by: Harsh Gupta <gupta.harsh96@gmail.com>

--- www/JBrowse/Util/SortAnnotationsByLocation.js
+++ www/JBrowse/Util/SortAnnotationsByLocation.js
@@ -4,11 +4,25 @@ define([], function () {
                                var start1 = annot1.get("start");
                                var end1 = annot1.get("end");
                                var start2 = annot2.get("start");
-                               var end2 = annot2.get('end');
+                               var end2 = annot2.get("end");
+                               var type1 = annot1.get("type");
+                               var type2 = annot2.get("type");
 
                                if (start1 != start2)  { return start1 - start2; }
                                else if (end1 != end2) { return end1 - end2; }
-                               else                   { return 0; }
+                               else                   {
+                                   if (type1 === type2) {
+                                       return 0;
+                                   }
+                                   else {
+                                       if(type1 < type2) {
+                                           return -1;
+                                       }
+                                       else {
+                                           return 1;
+                                       }
+                                   }
+                               }
                            });
     };
 });
